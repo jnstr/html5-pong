65720ac35ac35fbd7f300b7cccebb619dc7c24e6
Stagiair Jens <jens@jnstr.be>
2014-10-21 19:18:51 +0100

    move ball between player (only x)

--- src/js/pong.js
+++ src/js/pong.js
@@ -3,6 +3,10 @@
  */
 var pong = (function () {
     /**
+     * The interval for the game
+     */
+    interval = false;
+    /**
      * General configuration settings
      * @type Object
      */
@@ -32,7 +36,7 @@ var pong = (function () {
          * Init the game
          */
         init: function () {
-            this.user.init();
+            this.player.init();
             this.cpu.init();
             this.ball.init();
         },
@@ -44,9 +48,9 @@ var pong = (function () {
             stage.canvas.redraw();
         },
         /**
-         * The user (player)
+         * The player (player)
          */
-        user: {
+        player: {
             x: 0,
             y: 0,
             score: 0,
@@ -57,7 +61,7 @@ var pong = (function () {
             init: function () {
                 this.score = 0;
                 this.x = 4;
-                this.y = (config.canvas.width / 2) - (config.player.height / 2);
+                this.y = (config.canvas.height / 2) - (config.player.height / 2);
                 this.missed = false;
             }
         },
@@ -74,7 +78,7 @@ var pong = (function () {
              */
             init: function () {
                 this.x = config.canvas.width - 4 - config.player.width,
-                    this.y = (config.canvas.width / 2) - (config.player.height / 2),
+                    this.y = (config.canvas.height / 2) - (config.player.height / 2),
                     this.score = 0,
                     this.missed = false
             }
@@ -110,13 +114,23 @@ var pong = (function () {
              * Calculate the new position of the ball
              */
             calculate: function() {
-                if (game.ball.x <= 0) {
-                    game.ball.xDir = 1;
-                    game.ball.incrementSpeed();
-                }
-                if (game.ball.x >= (config.canvas.width - config.ball.size)) {
-                    game.ball.xDir = -1;
-                    game.ball.incrementSpeed();
+                switch (true) {
+                    case (game.ball.x <= game.player.x + config.player.width):
+                        game.ball.xDir = 1;
+                        game.ball.incrementSpeed();
+                        break;
+                    case (game.ball.x + config.ball.size >= game.cpu.x):
+                        game.ball.xDir = -1;
+                        game.ball.incrementSpeed();
+                        break;
+                    case (game.ball.x <= 0):
+                        alert('cpu won the game');
+                        window.clearInterval(pong.interval);
+                        break;
+                    case (game.ball.x >= (config.canvas.width - config.ball.size)):
+                        alert('user won the game');
+                        window.clearInterval(pong.interval);
+                        break;
                 }
 
                 game.ball.x = game.ball.x + (game.ball.xDir * game.ball.speed);
@@ -130,7 +144,9 @@ var pong = (function () {
     stage = {
         canvas : false,
         elements: {
-            ball: false
+            ball: false,
+            player: false,
+            cpu: false
         },
         init: function () {
             if (stage.canvas) {
@@ -143,20 +159,49 @@ var pong = (function () {
             stage.canvas.width= config.canvas.width;
             stage.canvas.height= config.canvas.height;
             stage.draw.ball();
+            stage.draw.player();
+            stage.draw.cpu();
         },
         draw: {
             ball: function() {
                 if (!stage.elements.ball) {
                     stage.elements.ball = stage.canvas.display.rectangle({
                         x: config.ball.x,
-                        y: game.ball.y,
+                        y: config.ball.y,
                         width: game.ball.radius,
                         height: game.ball.radius,
                         fill: config.ball.color
                     });
                 }
                 stage.canvas.addChild(stage.elements.ball);
+            },
+            player: function() {
+                if (!stage.elements.player) {
+                    stage.elements.player = stage.canvas.display.rectangle({
+                        x: game.player.x,
+                        y: game.player.y,
+                        width: config.player.width,
+                        height: config.player.height,
+                        fill: config.player.color
+                    });
+                    console.log(stage.elements.player);
+                }
+                stage.canvas.addChild(stage.elements.player);
+            },
+            cpu: function() {
+                if (!stage.elements.cpu) {
+                    stage.elements.cpu = stage.canvas.display.rectangle({
+                        x: game.cpu.x,
+                        y: game.cpu.y,
+                        width: config.player.width,
+                        height: config.player.height,
+                        fill: config.player.color
+                    });
+                    console.log(stage.elements.cpu);
+                }
+                stage.canvas.addChild(stage.elements.cpu);
             }
+
         }
     };
     /**
@@ -168,7 +213,7 @@ var pong = (function () {
          */
         play: function () {
             // repeat every 25 milliseconds = 25 frames/sec
-            timeout = setInterval(function() {
+            pong.interval = setInterval(function() {
                 game.update();
             }, 25);
 
